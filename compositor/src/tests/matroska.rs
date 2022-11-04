cb43b774b10365f2c0c38fdb4857c54a990752d1
Patrick Hoffmann <pat@thats-software.com>
2022-11-04 11:15:48 +0100

    add matroska only test

---
+++ compositor/src/tests/matroska.rs
@@ -0,0 +1,34 @@
+use crate::*;
+use core::time::Duration;
+use gstreamer as gst;
+
+#[test]
+fn test_matroska() {
+    env_logger::init();
+    // initialize gstreamer
+    gst::init().unwrap();
+
+    // get output resolution from arguments
+    let resolution = Size {
+        width: 640,
+        height: 480,
+    };
+
+    // use default parameters for sink
+    let sink_params = MatroskaParameters::default();
+
+    // create grid mixer with test sources for participants and a MatroskaSink
+    let mut mixer =
+        Mixer::<Grid, TestSource, MatroskaSink>::new(resolution, 8, 4, sink_params).unwrap();
+
+    // add a participant
+    mixer
+        .add_participant("test".into(), "".into(), Default::default())
+        .unwrap();
+
+    // start mixer
+    mixer.play();
+
+    // stir until done
+    std::thread::sleep(Duration::from_secs(10));
+}
--- compositor/src/tests/mod.rs
+++ compositor/src/tests/mod.rs
@@ -1,4 +1,5 @@
 const TEST_OUTPUT_DIR: &str = "test_output";
 
 mod dash;
+mod matroska;
 mod mixer;
